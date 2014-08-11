7cc7eb5b035e010b3b40199aa81d481babb37cae
andyqee <vitasone@gmail.com>
2014-08-11 16:40:41 +0800

    Initial Commit

---
+++ YQCustomViewTransitionDemo/YQAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  YQAppDelegate.h
+//  YQCustomViewTransitionDemo
+//
+//  Created by andy on 11/8/14.
+//  Copyright (c) 2014 andy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YQAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ YQCustomViewTransitionDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  YQCustomViewTransitionDemo
+//
+//  Created by andy on 11/8/14.
+//  Copyright (c) 2014 andy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "YQAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([YQAppDelegate class]));
+    }
+}
